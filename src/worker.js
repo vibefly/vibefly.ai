7fc6f4fcb7707ce4044c50ea0054005eabeb45b7
emcweeney <51935796+emcweeney@users.noreply.github.com>
2026-03-10 20:56:50 -0400

    chore: sync src/worker.js from template

--- src/worker.js
+++ src/worker.js
@@ -88,7 +88,7 @@ async function renderPage(env, url) {
         let tsHtml = '';
         if (formCfg.turnstileSiteKey) {
             const tsTheme = computeTurnstileTheme(content.theme);
-            tsHtml = `<div id="cf-turnstile" data-sitekey="${escAttr(formCfg.turnstileSiteKey)}" data-theme="${tsTheme}"></div>`;
+            tsHtml = `<div class="cf-turnstile" data-sitekey="${escAttr(formCfg.turnstileSiteKey)}" data-theme="${tsTheme}"></div>`;
         }
 
         const serviceAreas = (content.contact && content.contact.serviceAreas) || [];
